273b01abab79c2414c7c4120ed8e89dca80f7e99
Zefi93 <55651224+Zefi93@users.noreply.github.com>
2022-01-29 15:10:39 +0100

    Update OSDCloud.ps1

--- OSDCloud.ps1
+++ OSDCloud.ps1
@@ -22,8 +22,8 @@ switch ($input)
     '2' { Start-OSDCloud -OSLanguage fr-fr -OSBuild 20H2 -OSEdition Pro -ZTI }
     '3' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Pro -ZTI } 
     '4' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Pro -ZTI }
-    '5' { Start-OSDCloud	} 
-    '6' { Exit		}
+    '5' { Start-OSDCloud } 
+    '6' { Exit }
 }
 
 wpeutil reboot
